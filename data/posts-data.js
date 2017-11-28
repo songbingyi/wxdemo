bbecf8e6ef97d5575f5bcad482afd3e327ffd645
songbingyi <43463006@qq.com>
2017-11-28 17:38:14 +0800

    11.28

--- data/posts-data.js
+++ data/posts-data.js
@@ -1,39 +1,52 @@
 var local_database = [{
-  date: "Nov 25 20171111",
-  title: "机车DVA效果大片登场11",
-  post_image: "/images/1.jpg",
-  view_num: "1111",
-  collect: "1111",
-  postid: 1,
-  headImgSrc: "/images/1.jpg",
-  author: "林白衣",
-  postTime: "24小时前"
+    date: "Nov 25 20171111",
+    title: "机车DVA效果大片登场11",
+    post_image: "/images/1.jpg",
+    view_num: "1111",
+    collect: "1111",
+    postid: 0,
+    headImgSrc: "/images/4.jpg",
+    author: "林白衣1",
+    postTime: "24小时前",
+    content: "这其中不得不说的就是立下汗马功劳的绝地求生(PUBG)。这个以今晚吃鸡为目的的苟活类游戏年初上线后立即在全世界范围内火得一塌糊涂(霸占steam销量榜冠军N周，打破N项纪录)，几乎要复制十几年前CS火遍全中国网吧的奇迹。"
 },
 {
-  date: "Nov 25 20172",
-  title: "机车DVA效果大片登场2",
-  post_image: "/images/2.jpg",
-  view_num: "2222",
-  collect: "2222",
-  postid: 2
+    date: "Nov 25 20172",
+    title: "机车DVA效果大片登场2",
+    post_image: "/images/2.jpg",
+    view_num: "2222",
+    collect: "2222",
+    postid: 1,
+    headImgSrc: "/images/2.jpg",
+    author: "林白衣2",
+    postTime: "22小时前",
+    content: "面对此消息，很多网友表示OGN太可怜了，他们精心制作的LOL联赛已经被拳头拿走了，现在就连守望先锋联赛也会被暴雪拿走。。"
 },
 {
-  date: "3333",
-  title: "333333",
-  post_image: "/images/3.jpg",
-  view_num: "333",
-  collect: "3333",
-  postid: 3
+    date: "3333",
+    title: "333333",
+    post_image: "/images/3.jpg",
+    view_num: "333",
+    collect: "3333",
+    postid: 2,
+    headImgSrc: "/images/3.jpg",
+    author: "林白衣3",
+    postTime: "26小时前",
+    content: "目前来看，OGN制作APEX的质量比暴雪制作Contenders和世界杯的质量高很多，因此网友对于明年APEX的联赛质量表示担忧，甚至希望暴雪可以雇用OGN制作APEX的团队。"
 },
 {
-  date: "444",
-  title: "444",
-  post_image: "/images/3.jpg",
-  view_num: "44",
-  collect: "444",
-  postid: 4
+    date: "444",
+    title: "444",
+    post_image: "/images/3.jpg",
+    view_num: "44",
+    collect: "444",
+    postid: 3,
+    headImgSrc: "/images/3.jpg",
+    author: "林白衣4",
+    postTime: "27小时前",
+    content: "目前来看，OGN制作APEX的质量比暴雪制作Contenders和世界杯的质量高很多，因此网友对于明年APEX的联赛质量表示担忧，甚至希望暴雪可以雇用OGN制作APEX的团队。"
 }]
 
 module.exports = {
-  postlist: local_database
+    postlist: local_database
 }
--- page/post/post-detail/post-detail.js
+++ page/post/post-detail/post-detail.js
@@ -1,4 +1,5 @@
 // page/post/post-detail/post-detail.js
+var postData = require("../../../data/posts-data.js");
 Page({
 
   /**
@@ -12,8 +13,9 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-      var postid = options.id;
-      console.log(postid);
+      var postdatas = postData.postlist[options.id];
+      console.log(postData);
+      this.setData({postdatas});
       
   },
 
